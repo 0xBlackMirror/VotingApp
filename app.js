24b44d3d2ac01934255e89956946fbf2f8e44502
WackThat <martin50679@gmail.com>
2017-12-02 22:22:48 +0200

    Module installation and adding views folder

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 // Basic Node Requires
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 // Express App
 const app = express();
+const router = express.Router();
 // BodyParser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Mongoose Setup
-mognoose.connect('mongodb://localhost/votingapp');
+mongoose.connect('mongodb://localhost/votingapp');
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Successfully Connected To The Database.')
+});
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+// Apply Routes To '/'
+app.use('/', router);
+// HomePage
+router.get('/', (req, res) => {
+  res.render('view');
+});
+// Server Start
+app.listen(process.env.PORT || 3000, () => {
+	console.log('Server Is Online');
 });
