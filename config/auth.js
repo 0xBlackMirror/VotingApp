42725be5cff1fef9c58956c45f9f41cfeddad19a
WackThat <martin50679@gmail.com>
2017-12-20 16:09:35 +0200

    last one i swear

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	'google' : {
-		clientID: '467824594128-1cgstme1p717sjsueh6attet3upbmpja.apps.googleusercontent.com',
+		clientID: '467824594128-39sqt4rbdtlau13cbq5309ngep6sak92.apps.googleusercontent.com',
 		callbackURL: 'http://votingapp-wack.herokuapp.com/auth/google/callback'
 	},
 }
